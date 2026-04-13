bb7c887b36aee03e7bfedef7b3a1cab021816538
ojovio <jotaviocmello@gmail.com>
2026-04-13 17:35:47 -0300

    aaaaaa fisica

---
+++ include/tilemap/chunk_streaming.h
@@ -0,0 +1,16 @@
+//
+// Created by j-otavio on 13/04/2026.
+//
+
+#ifndef STONEWAKE_CHUNK_STREAMING_H
+#define STONEWAKE_CHUNK_STREAMING_H
+
+
+
+class chunk_streaming {
+
+};
+
+
+
+#endif //STONEWAKE_CHUNK_STREAMING_H
---
+++ src/tilemap/chunk_streaming.cpp
@@ -0,0 +1,5 @@
+//
+// Created by j-otavio on 13/04/2026.
+//
+
+#include "../../include/tilemap/chunk_streaming.h"
